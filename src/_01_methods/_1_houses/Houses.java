83b1f292d156c81ab721775efe188dbaa6d37f35
league <maddoxseul@sbsd.net>
2026-07-05 12:56:50 -0700

    awooo

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +10,13 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+		Robot robby = new Robot();
+		robby.moveTo(0, 500);
+		robby.hide();
 	}
+		
+	void drawHouse(int height, boolean pointy, String color) {
+		System.out.println("drawing house...");
+	}
+	
 }
